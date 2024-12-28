845e610501412a5bbff106fba2be47660faa4c3a
ElloRabyndra <ElloRabyndra123@gmail.com>
2024-12-28 18:48:56 +0700

    finishing

--- js/script.js
+++ js/script.js
@@ -22,3 +22,77 @@ window.addEventListener("scroll", () => {
     header.classList.remove("nav-glass");
   }
 });
+
+// Contact Form
+const contactForm = document.querySelector("form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+
+contactForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  clearErrors();
+
+  let isValid = true;
+
+  if (nameInput.value.trim() === "") {
+    showError(nameInput, "Name is required.");
+    isValid = false;
+  }
+
+  if (emailInput.value.trim() === "") {
+    showError(emailInput, "Email is required.");
+    isValid = false;
+  } else if (!validateEmail(emailInput.value.trim())) {
+    showError(emailInput, "Invalid email.");
+    isValid = false;
+  }
+
+  if (messageInput.value.trim() === "") {
+    showError(messageInput, "Message is required.");
+    isValid = false;
+  }
+
+  if (isValid) {
+    showSuccess();
+    contactForm.submit();
+    contactForm.reset();
+  }
+});
+
+//  menampilkan Error
+function showError(input, message) {
+  const error = document.createElement("div");
+  error.className = "error-message";
+  error.textContent = message;
+  input.classList.add("error");
+  input.parentElement.appendChild(error);
+
+  setTimeout(() => {
+    error.remove();
+    input.classList.remove("error");
+  }, 3000);
+}
+
+// Pesan Terkirim
+function showSuccess() {
+  const success = document.getElementById("success-message");
+  console.log(success);
+  success.classList.remove("hidden");
+  setTimeout(() => {
+    success.classList.add("hidden");
+  }, 3000);
+}
+
+function clearErrors() {
+  const errorMessages = document.querySelectorAll(".error-message");
+  errorMessages.forEach((error) => error.remove());
+  const inputs = document.querySelectorAll("input, textarea");
+  inputs.forEach((input) => input.classList.remove("error"));
+}
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(String(email).toLowerCase());
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,15 @@ module.exports = {
         main: "#10b981",
         secondary: "#334155",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateY(20px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in": "fadeIn 1.2s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
